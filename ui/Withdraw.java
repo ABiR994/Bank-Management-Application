1ea48ff6923ea56564fe999c122a163b4d69d89b
ABiR994 <salemnurabir2@gmail.com>
2026-01-01 15:44:25 +0600

    Withdraw Logic added

--- ui/Withdraw.java
+++ ui/Withdraw.java
@@ -5,7 +5,7 @@ import java.awt.event.*;
 import utils.*;
 
 public class Withdraw extends MainFrame {
-    public Withdraw(String accountData) {
+    public Withdraw(String account) {
         super("Withdraw", "Enter Amount to Withdraw", 550, 400, false, 0, 110, 550, 30);
 
         //! Label and TextField
@@ -19,13 +19,38 @@ public class Withdraw extends MainFrame {
         JButton btnWithdraw = Utils.createButton("Withdraw", 150, 250, 100, 40);
         panel.add(btnWithdraw);
 
-        JButton btnExit = Utils.createButton("Exit", 270, 250, 100, 40);
-        panel.add(btnExit);
+        JButton btnBack = Utils.createButton("Back", 270, 250, 100, 40);
+        panel.add(btnBack);
 
-        btnExit.addActionListener(e -> this.dispose());
+        btnBack.addActionListener(e -> {
+            this.dispose();
+            new Transaction(account);
+        });
 
         btnWithdraw.addActionListener(e -> {
-            // handle withdraw logic here
+
+            if(withdrawField.getText().isEmpty()) {
+                Utils.showMessage(this, "Please enter a withdraw amount!", "Error", 350);
+                return;
+            }
+
+            try {
+                double amount = Double.parseDouble(withdrawField.getText());
+                if(amount > 0) {
+                    if(account.withdraw(amount)) {
+                        AccountFileHandler.updateAccount(account);
+                        Utils.updatedBalanceMessage(this, "Withdraw Successful!", "Updated Balance: " + account.getBalance(), "Success");
+                        this.dispose();
+                        new Transaction(account);
+                    } else {
+                        Utils.showMessage(this, "Insufficient balance!", "Error", 350);
+                    }
+                } else {
+                    Utils.showMessage(this, "Invalid amount!", "Error", 350);
+                }
+            } catch(NumberFormatException ex) {
+                Utils.showMessage(this, "Enter a valid number!", "Error", 350);
+            }
         });
 
         this.setVisible(true);
