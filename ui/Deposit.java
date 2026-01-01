2b3fe6c4360c8ad45ed6772ae8800366b9eb4ae4
ABiR994 <salemnurabir2@gmail.com>
2026-01-01 15:41:07 +0600

    minor issue fixed

--- ui/Deposit.java
+++ ui/Deposit.java
@@ -24,7 +24,7 @@ public class Deposit extends MainFrame {
         panel.add(btnBack);
 
         btnBack.addActionListener(e -> {
-            this.dispose()
+            this.dispose();
             new Transaction(accountData);
         });
 
