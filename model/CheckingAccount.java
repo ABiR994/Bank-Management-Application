453b83ab263ba6ce6ce1cc0b782a57733dc3e794
ABiR994 <salemnurabir2@gmail.com>
2026-01-01 15:10:27 +0600

    Minimum Balance and Overdraft Limit logic added

--- model/CheckingAccount.java
+++ model/CheckingAccount.java
@@ -1,6 +1,9 @@
 package model;
 
 public class CheckingAccount extends Account {
+
+    private static final double OVERDRAFT_LIMIT = -1000;
+
     public CheckingAccount(String acc, String name, String pass, String email, String phoneNum,double balance) {
         super(acc, name, pass, email, phoneNum, balance);
     }
@@ -8,5 +11,14 @@ public class CheckingAccount extends Account {
     @Override
     public String getType() {
         return "Checking";
+    }
+
+    @Override
+    public boolean canWithdraw(double amount) {
+        if(amount > 0 && (balance - amount) >= OVERDRAFT_LIMIT) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- model/SavingsAccount.java
+++ model/SavingsAccount.java
@@ -1,6 +1,9 @@
 package model;
 
 public class SavingsAccount extends Account {
+
+    private static final double MIN_BALANCE = 500;
+
     public SavingsAccount(String acc, String name, String pass, String email, String phoneNum,double balance) {
         super(acc, name, pass, email, phoneNum, balance);
     }
@@ -8,5 +11,14 @@ public class SavingsAccount extends Account {
     @Override
     public String getType() {
         return "Savings";
+    }
+
+    @Override
+    public boolean canWithdraw(double amount) {
+        if(amount > 0 && (balance - amount) >= MIN_BALANCE) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
